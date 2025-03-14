afabc7b71b363f2457d8d5d9aadca8e5952ea935
mikhailkhorokhorin <mikhailkhorokhorin@gmail.com>
2025-03-14 20:09:39 +0300

    Refactor case converter algorithm

--- app/utils/case_converter.py
+++ app/utils/case_converter.py
@@ -1,11 +1,11 @@
-def camel_case_to_snake_case(input_str: str) -> str:
-    chars = []
-    for c_idx, char in enumerate(input_str):
-        if c_idx and char.isupper():
-            nxt_idx = c_idx + 1
-            flag = nxt_idx >= len(input_str) or input_str[nxt_idx].isupper()
-            prev_char = input_str[c_idx - 1]
+def camel_case_to_snake_case(camel: str) -> str:
+    snake = []
+    for index, char in enumerate(camel):
+        if index and char.isupper():
+            next_index = index + 1
+            flag = next_index >= len(camel) or camel[next_index].isupper()
+            prev_char = camel[index - 1]
             if not (prev_char.isupper() and flag):
-                chars.append("_")
-        chars.append(char.lower())
-    return "".join(chars)
+                snake.append("_")
+        snake.append(char.lower())
+    return "".join(snake)
