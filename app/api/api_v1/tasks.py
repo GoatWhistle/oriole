5644e738253ec3780be5d3bb5ac552edfcc9c981
amemeansrain <savrobartrobsav@gmail.com>
2025-03-19 20:17:20 +0300

    Add Task API

--- app/api/api_v1/tasks.py
+++ app/api/api_v1/tasks.py
@@ -1,3 +1,106 @@
-from fastapi import APIRouter
+from fastapi import (
+    APIRouter,
+    Depends,
+    status,
+)
 
-router = APIRouter()
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.annotation import Annotated
+
+from core.config import settings
+from core.models import db_helper, Task
+from core.schemas.task import (
+    TaskCreate,
+    TaskRead,
+    TaskUpdate,
+    TaskUpdatePartial,
+)
+from crud import tasks as crud
+
+router = APIRouter(tags=settings.api.v1.tasks.capitalize())
+
+
+@router.post(
+    "/",
+    response_model=TaskRead,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_task(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    task_in: TaskCreate,
+):
+    return await crud.create_task(session=session, task_in=task_in)
+
+
+@router.get(
+    "/{task_id}/",
+    response_model=TaskRead,
+)
+async def get_task(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    task_id: int,
+):
+    return await crud.get_task(session=session, task_id=task_id)
+
+
+@router.get(
+    "/",
+    response_model=list[TaskRead],
+)
+async def get_tasks(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.dependency_session_getter()),
+    ],
+):
+    return await crud.get_tasks(session=session)
+
+
+@router.put("/{task_id}/")
+async def update_task(
+    session: Annotated[AsyncSession, Depends(db_helper.dependency_session_getter())],
+    task_update: TaskUpdate,
+    task: Task,
+):
+    return await crud.update_task(
+        session=session,
+        task=task,
+        task_update=task_update,
+    )
+
+
+@router.patch("/{task_id}/")
+async def update_task_partial(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    task_update: TaskUpdatePartial,
+    task: Task,
+):
+    return await crud.update_task(
+        session=session,
+        task=task,
+        task_update=task_update,
+        partial=True,
+    )
+
+
+@router.delete(
+    "/{task_id}/",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_task(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    task: Task,
+) -> None:
+    await crud.delete_task(session=session, task=task)
