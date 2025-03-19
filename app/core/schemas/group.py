a0d26c840cf3d9d7110798a4be32c55196ba2c15
mikhailkhorokhorin <mikhailkhorokhorin@gmail.com>
2025-03-19 16:18:50 +0300

    Add group updating logic

--- app/core/schemas/group.py
+++ app/core/schemas/group.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Field
-from typing import Annotated
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Annotated, Optional
 
 from .user import UserRead
 from .task import TaskRead
@@ -7,6 +7,7 @@ from .task import TaskRead
 
 class GroupBase(BaseModel):
     title: Annotated[str, Field(max_length=64)]
+    description: Annotated[str, Field(max_length=200)]
     admin: UserRead
     admin_id: int
 
@@ -15,7 +16,20 @@ class GroupCreate(GroupBase):
     pass
 
 
+class GroupUpdate(GroupCreate):
+    pass
+
+
+class GroupUpdatePartial(GroupCreate):
+    title: Annotated[Optional[str], Field(max_length=64)] = None
+    description: Annotated[Optional[str], Field(max_length=200)] = None
+    admin: Optional[UserRead] = None
+    admin_id: Optional[int] = None
+
+
 class GroupRead(GroupBase):
     id: int
     users: list[UserRead] = []
     tasks: list[TaskRead] = []
+
+    model_config = ConfigDict(from_attributes=True)
