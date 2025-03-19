b9769f2a943c9978e08ecac7dd5ba8b66c62c035
amemeansrain <savrobartrobsav@gmail.com>
2025-03-19 20:17:11 +0300

    Add Task Schemas

--- app/core/schemas/task.py
+++ app/core/schemas/task.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Field
-from typing import Annotated
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Annotated, Optional
 
 from .group import GroupRead
 from .user import UserRead
@@ -23,3 +23,23 @@ class TaskCreate(TaskBase):
 
 class TaskRead(TaskBase):
     id: int
+
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
+
+
+class TaskUpdate(TaskCreate):
+    pass
+
+
+class TaskUpdatePartial(TaskCreate):
+    name: Annotated[Optional[str], Field(max_length=100)]
+    text: Optional[str]
+    correct_answer: Optional[str]
+
+    group: Optional[GroupRead]
+    group_id: Optional[int]
+
+    admin: Optional[UserRead]
+    admin_id: Optional[int]
