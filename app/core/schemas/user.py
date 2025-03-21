6184e1df54fa49e8389ad9a7f367d2220a1c1161
artems <whard8y@gmail.com>
2025-03-21 10:27:39 +0300

    Сreate user schemas

--- app/core/schemas/user.py
+++ app/core/schemas/user.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel, EmailStr, Field
 from typing import Annotated
+from fastapi_users import schemas
 
 from .group import GroupBase
 from .task import TaskRead
+from core.types.user_id import UserIdType
 
 
+# TODO: обратить внимание
 class UserBase(BaseModel):
     name: Annotated[str, Field(max_length=32)]
     surname: Annotated[str, Field(max_length=32)]
@@ -14,12 +17,21 @@ class UserBase(BaseModel):
     password: str
 
 
-class UserCreate(UserBase):
-    pass
-
-
-class UserRead(UserBase):
+class UserRead(schemas.BaseUser[UserIdType]):
     id: int
     groups: list[GroupBase | None]
     admin_groups: list[GroupBase | None]
     admin_tasks: list[TaskRead | None]
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
+
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+
+class UserRegisteredNotification(BaseModel):
+    user: UserRead
+    ts: int
