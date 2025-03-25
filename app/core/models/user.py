f9a890ddd62a79671a962ba6afe844a58d0640a5
pupyk <pupykint@gmail.com>
2025-03-25 18:13:26 +0300

    Divide User to User and UserProfile

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -4,39 +4,19 @@ from fastapi_users_db_sqlalchemy import (
     SQLAlchemyUserDatabase,
     SQLAlchemyBaseUserTable,
 )
+from sqlalchemy.orm import Mapped, relationship
 
-from .user_group_association import user_group_association_table
-
-from sqlalchemy import String
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column,
-    relationship,
-)
-
+from core.models.user_profile import UserProfile
 from core.types.user_id import UserIdType
 from .base import Base
 from .mixins.id_int_pk import IdIntPkMixin
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
-    from .group import Group
-    from .task import Task
 
 
 class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
-    name: Mapped[str] = mapped_column(String(31))
-    surname: Mapped[str] = mapped_column(String(31))
-    patronymic: Mapped[str] = mapped_column(String(63))
-
-    groups: Mapped[list[Optional["Group"]]] = relationship(
-        secondary=user_group_association_table,
-        back_populates="users",
-    )
-
-    admin_groups: Mapped[list[Optional["Group"]]] = relationship(back_populates="admin")
-
-    admin_tasks: Mapped[list[Optional["Task"]]] = relationship(back_populates="admin")
+    profile: Mapped["UserProfile"] = relationship(back_populates="user")
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
---
+++ app/core/models/user_profile.py
@@ -0,0 +1,38 @@
+from typing import TYPE_CHECKING, Optional
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
+
+from .base import Base
+from .user_group_association import user_group_association_table
+
+if TYPE_CHECKING:
+    from .group import Group
+    from .task import Task
+    from .user import User
+
+
+class UserProfile(Base):
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        primary_key=True,
+    )
+    user: Mapped["User"] = relationship(back_populates="profile")
+
+    name: Mapped[str] = mapped_column(String(31), index=True)
+    surname: Mapped[str] = mapped_column(String(31), index=True)
+    patronymic: Mapped[str] = mapped_column(String(63), index=True)
+
+    groups: Mapped[list[Optional["Group"]]] = relationship(
+        secondary=user_group_association_table,
+        back_populates="users",
+    )
+
+    admin_groups: Mapped[list[Optional["Group"]]] = relationship(back_populates="admin")
+
+    admin_tasks: Mapped[list[Optional["Task"]]] = relationship(back_populates="admin")
