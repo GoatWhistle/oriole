4119cb9de1648c7ac69d739878b58f953eca3f56
pupyk <pupykint@gmail.com>
2025-05-11 21:06:03 +0300

    Add CORS

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.events import lifespan
 from core.config import settings
@@ -8,6 +9,17 @@ from core.config import settings
 from api import router as api_router
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://frontend:80",
+        "http://127.0.0.1:80",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     api_router,
