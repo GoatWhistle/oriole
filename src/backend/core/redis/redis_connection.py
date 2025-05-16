a8d07341e0b221d2cc494d9e824084ef5f3f663c
pupyk <pupykint@gmail.com>
2025-05-16 13:54:40 +0300

    Choose differ db's for redis

--- src/backend/core/redis/redis_connection.py
+++ src/backend/core/redis/redis_connection.py
@@ -3,16 +3,17 @@ from core.config import settings
 
 
 class RedisConnection:
-    def __init__(self, url: str):
+    def __init__(self, url: str, db: int):
         self.url = url
         self.redis = None
+        self.db = db
 
     async def connect(self):
-        self.redis = await aioredis.from_url(self.url)
+        self.redis = await aioredis.from_url(self.url, self.db)
 
     async def close(self):
         if self.redis:
             await self.redis.close()
 
 
-redis_connection = RedisConnection(f"{settings.redis.url}{settings.redis.port}")
+redis_connection = RedisConnection(f"{settings.redis.url}{settings.redis.port}", 0)
