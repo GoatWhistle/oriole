3aca11ce58b106792eae77bff0d5c5f1557c56b3
amemeansrain <savrobartrobsav@gmail.com>
2025-05-16 11:40:56 +0300

    Test visual for Group Page

---
+++ src/frontend/src/pages/Group.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from "/src/components/Header.jsx";
+import GroupUsersMenu from "/src/components/GroupUsersMenu.jsx";
+
+export default function Profile() {
+  return <div>
+            <Header />
+            <GroupUsersMenu />
+         </div>
+}
