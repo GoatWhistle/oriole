0b44a797fc9791a27de7d75fa2ac7aecf77e9dfb
pupyk <pupykint@gmail.com>
2025-05-17 14:27:06 +0300

    Move out migrations😎

--- src/backend/pre_start.sh
+++ src/backend/pre_start.sh
@@ -1,9 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -e
+DB_HOST=$(echo "$APP_CONFIG__DB__DB_URL" | sed -E 's|.+@([^:/]+):.*|\1|')
+DB_PORT=$(echo "$APP_CONFIG__DB__DB_URL" | sed -E 's|.+:([0-9]+)/.*|\1|')
 
-echo "Run apply migrations..."
+echo "Waiting connection to db ${DB_HOST}:${DB_PORT}..."
+
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
+  echo "DB_URL: $APP_CONFIG__DB__DB_URL"
+  echo "DB_HOST: $DB_HOST"
+  echo "DB_PORT: $DB_PORT"
+  echo "db not available..."
+  sleep 1
+done
+
+echo "db available"
+
 alembic upgrade head
-echo "Migrations applied!"
 
-exec "$@"
+echo "Migrations completed"
