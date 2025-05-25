3ff76c025171649139720ec5b44b65495fe3a997
amemeansrain <savrobartrobsav@gmail.com>
2025-05-26 01:51:50 +0300

    Added new routers and refactor old ones

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -23,11 +23,12 @@ export default function App() {
         <Route path="/registration" element={<Registration />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/verify/:token" element={<Verify />} />
-        <Route path="/assignment/:assignment_id" element={<Assignment />} />
-        <Route path="/group/:group_id" element={<Group />} />
+        <Route path="/assignments/:assignment_id" element={<Assignment />} />
+        <Route path="/groups/:group_id" element={<Group />} />
         <Route path="/groups/join/:invite_code" element={<JoinGroup />} />
         <Route path="/forgot_password_redirect/:token" element={<ForgotPassword />} />
         <Route path="/reset_password_redirect/:token" element={<ResetPassword />} />
+        <Route path="/tasks/:task_id" element={<Task />} />
       </Routes>
     </div>
   );
