5886f184c89da68e12f3869d140c8b7cc526f9dd
amemeansrain <savrobartrobsav@gmail.com>
2025-05-26 01:51:19 +0300

    Added Task Page

---
+++ src/frontend/src/pages/Task.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from "/src/components/Header.jsx";
+import TaskDetails from "/src/components/TaskDetails.jsx";
+
+export default function Task() {
+  return <div>
+            <Header/>
+            <TaskDetails />
+         </div>
+}
