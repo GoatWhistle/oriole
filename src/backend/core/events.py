8d8a6428132ceaa793fdc79d18e67b14c0035b30
Mikhail Khorokhorin <mikhailkhorokhorin@gmail.com>
2025-05-31 01:10:56 +0300

    Remove old background tasks logic

--- src/backend/core/events.py
+++ src/backend/core/events.py
@@ -2,19 +2,14 @@ from fastapi import FastAPI
 from core.redis import redis_connection
 from core.models.db_helper import db_helper
 from contextlib import asynccontextmanager
-from .backgorund_tasks import scheduler
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await redis_connection.connect()
-    if not scheduler.running:
-        scheduler.start()
 
     yield
 
-    if scheduler.running:
-        scheduler.shutdown(wait=False)
     await redis_connection.close()
     await db_helper.dispose()
 
