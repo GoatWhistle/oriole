b400510bf1c597b1f6971ef98e76c23ed2e56855
amemeansrain <savrobartrobsav@gmail.com>
2025-05-31 04:08:35 +0300

    App context changed

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -17,20 +17,20 @@ import Task from './pages/Task';
 export default function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hello" element={<Hello />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/verify/:token" element={<Verify />} />
-        <Route path="/assignments/:assignment_id" element={<Assignment />} />
-        <Route path="/groups/:group_id" element={<Group />} />
-        <Route path="/groups/join/:invite_code" element={<JoinGroup />} />
-        <Route path="/forgot_password_redirect/:token" element={<ForgotPassword />} />
-        <Route path="/reset_password_redirect/:token" element={<ResetPassword />} />
-        <Route path="/tasks/:task_id" element={<Task />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hello" element={<Hello />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/verify/:token" element={<Verify />} />
+          <Route path="/assignments/:assignment_id" element={<Assignment />} />
+          <Route path="/groups/:group_id" element={<Group />} />
+          <Route path="/groups/join/:invite_code" element={<JoinGroup />} />
+          <Route path="/forgot_password_redirect/:token" element={<ForgotPassword />} />
+          <Route path="/reset_password_redirect/:token" element={<ResetPassword />} />
+          <Route path="/tasks/:task_id" element={<Task />} />
+        </Routes>
     </div>
   );
 }
