9047c198ddbfe8ebb30090c8f5febad049914e92
amemeansrain <savrobartrobsav@gmail.com>
2025-06-13 17:33:54 +0300

    Moved components directory to features directory, added api and handlers directories and changed paths

--- src/frontend/src/pages/Module.jsx
+++ src/frontend/src/pages/Module.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from "/src/components/Header.jsx";
-import ModuleDetails from "/src/components/ModuleDetails.jsx";
+import Header from "/src/features/components/Header.jsx";
+import ModuleDetails from "/src/features/components/ModuleDetails.jsx";
 
 export default function Module() {
   return <div>
--- src/frontend/src/pages/Profile.jsx
+++ src/frontend/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from "/src/components/Header.jsx";
-import UserProfile from "/src/components/UserProfile.jsx";
+import Header from "/src/features/components/Header.jsx";
+import UserProfile from "/src/features/components/UserProfile.jsx";
 
 export default function Profile() {
   return <div>
