c6a039e976edf8f4f558cf676f54a10bd0eb49d7
artems <whard8y@gmail.com>
2025-06-21 12:28:13 +0300

    Fix bugs with different exceptions in limiter handler

--- src/backend/core/events.py
+++ src/backend/core/events.py
@@ -6,16 +6,15 @@ from core.redis import redis_connection
 from database import db_helper
 
 from middlewares.limiter import limiter
-from slowapi.errors import RateLimitExceeded
 from core.config import settings
-from middlewares.limiter import custom_rate_limit_handler
+from middlewares.limiter import universal_rate_limit_handler
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     if settings.redis.limiter_enabled:
         app.state.limiter = limiter
-        app.add_exception_handler(RateLimitExceeded, custom_rate_limit_handler)
+        app.add_exception_handler(Exception, universal_rate_limit_handler)
 
     await redis_connection.connect()
 
