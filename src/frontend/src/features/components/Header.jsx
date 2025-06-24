2a1eaa8268eea07491be6fdb3622195de82943d1
amemeansrain <savrobartrobsav@gmail.com>
2025-06-24 21:32:07 +0300

    Add coloring, border, hover behavior to Header

--- src/frontend/src/features/components/Header.jsx
+++ src/frontend/src/features/components/Header.jsx
@@ -1,42 +1,36 @@
-import { Card } from "antd";
-import orioleIcon from './oriole-icon.png';
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 
+import orioleIcon from './oriole-icon.png';
 import AuthButton from './AuthButton.jsx'
+import styles from './Header.module.css';
+
 
 function Header() {
-
   useEffect(() => {
-        document.title = 'Oriole';
-      });
+    document.title = 'Oriole';
+  });
 
   return (
-    <Card
-      style={{ width: '100%' }}
-      bodyStyle={{ padding: 0, display: 'flex', alignItems: 'center' }}
+    <div
+      className={styles.headerCard}
     >
-      <div style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
-        <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-            <Link
-              to="/"
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '10px',
-                color: 'black',
-                textDecoration: 'none'
-              }}
-            >
-                <img src={orioleIcon} alt="Oriole Icon" width="80" height="80" />
-                <span style={{ fontSize: '30px', color: 'black' }}>Oriole</span>
-            </Link>
+      <div className={styles.headerContainer}>
+        <div className={styles.logoContainer}>
+          <Link
+            to="/"
+            className={styles.logoLink}
+          >
+            <img className={styles.image} src={orioleIcon} alt="Oriole Icon" width="80" height="80" />
+            <span className={styles.logoText}>Oriole</span>
+          </Link>
         </div>
 
-        <AuthButton />
-
+        <div className={styles.authButtonContainer}>
+          <AuthButton />
+        </div>
       </div>
-    </Card>
+    </div>
   )
 }
 
