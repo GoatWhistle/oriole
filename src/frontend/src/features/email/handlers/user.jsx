57dc1101c0136d5d247f4f926d3eb509f3c0672a
amemeansrain <savrobartrobsav@gmail.com>
2025-06-28 23:06:57 +0300

    Add handleForgotPassword handler func for ForgotPasswordRedirect component

---
+++ src/frontend/src/features/email/handlers/user.jsx
@@ -0,0 +1,18 @@
+import { forgotPassword } from '../api/user.jsx';
+import { fetchError } from '../../api/error.jsx';
+
+export const handleForgotPassword = async (token, newPassword, navigate, setLoading) => {
+  try {
+    setLoading(true);
+    const response = await forgotPassword(token, newPassword);
+
+    if (response.status === 'success') {
+      navigate('/');
+      return true;
+    }
+  } catch (error) {
+      fetchError(error, 'Ошибка при восстановлении пароля');
+  } finally {
+    setLoading(false);
+  }
+};
