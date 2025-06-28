b8fef62e600cd80b8aa30bb8dbfc033752ff7257
amemeansrain <savrobartrobsav@gmail.com>
2025-06-28 23:06:24 +0300

    Add forgotPassword API func for ForgotPasswordRedirect component

---
+++ src/frontend/src/features/email/api/user.jsx
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const forgotPassword = async (token, newPassword) => {
+  const response = await axios.get(
+    `/api/verify/forgot_password_redirect/${token}`,
+    {
+      params: { new_password: newPassword },
+      withCredentials: true
+    }
+  );
+  return response.data;
+};
