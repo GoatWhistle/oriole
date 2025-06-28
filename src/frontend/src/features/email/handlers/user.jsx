8c0cc3cb58db9b68ae022025707c13123ddcfaa6
amemeansrain <savrobartrobsav@gmail.com>
2025-06-28 23:33:59 +0300

    Add handleResetPassword handler func for ResetPasswordRedirect component

--- src/frontend/src/features/email/handlers/user.jsx
+++ src/frontend/src/features/email/handlers/user.jsx
@@ -1,4 +1,4 @@
-import { forgotPassword } from '../api/user.jsx';
+import { forgotPassword, resetPassword } from '../api/user.jsx';
 import { fetchError } from '../../api/error.jsx';
 
 export const handleForgotPassword = async (token, newPassword, navigate, setLoading) => {
@@ -14,5 +14,21 @@ export const handleForgotPassword = async (token, newPassword, navigate, setLoad
       fetchError(error, 'Ошибка при восстановлении пароля');
   } finally {
     setLoading(false);
+  }
+};
+
+export const handleResetPassword = async (token, newPassword, navigate, setLoading) => {
+  try {
+    setLoading(true);
+    const response = await resetPassword(token, newPassword);
+
+    if (response.status === 'success') {
+      navigate('/');
+      return true;
+    }
+  } catch (error) {
+      fetchError(error, error.response ? 'Ошибка сервера' : 'Ошибка сети');
+  } finally {
+      setLoading(false);
   }
 };
