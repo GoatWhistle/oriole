8be6054969ebb9a21d738fd4d6ff4b71963946bb
amemeansrain <savrobartrobsav@gmail.com>
2025-06-29 00:19:31 +0300

    Fix path for Verified component in Verify page

--- src/frontend/src/pages/Verify.jsx
+++ src/frontend/src/pages/Verify.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "/src/features/components/Header/Header.jsx";
-import Verified from "/src/features/components/Verified.jsx";
+import Verified from "/src/features/email/components/Verified/Verified.jsx";
 
 export default function Verify() {
   return <div>
