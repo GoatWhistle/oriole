8ba4b7517621ce3c5bf40b187a12c5689ab86d99
amemeansrain <savrobartrobsav@gmail.com>
2025-06-28 23:33:40 +0300

    Add resetPassword API func for ResetPasswordRedirect component

--- src/frontend/src/features/email/api/user.jsx
+++ src/frontend/src/features/email/api/user.jsx
@@ -10,3 +10,14 @@ export const forgotPassword = async (token, newPassword) => {
   );
   return response.data;
 };
+
+export const resetPassword = async (token, newPassword) => {
+  const response = await axios.get(
+    `/api/verify/reset_password_redirect/${token}`,
+    {
+      params: { new_password: newPassword },
+      withCredentials: true
+    }
+  );
+  return response.data;
+};
