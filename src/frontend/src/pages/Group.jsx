3ea474601aa5c0d1b2fa3b95864fba5ade2ae9e1
amemeansrain <savrobartrobsav@gmail.com>
2025-06-28 21:57:59 +0300

    Fix path for GroupDetails component in Group page

--- src/frontend/src/pages/Group.jsx
+++ src/frontend/src/pages/Group.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "/src/features/components/Header/Header.jsx";
-import GroupDetails from "/src/features/groups/GroupDetails.jsx";
+import GroupDetails from "/src/features/groups/components/GroupDetails/GroupDetails.jsx";
 
 export default function Group() {
   return <div>
