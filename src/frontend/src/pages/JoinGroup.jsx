c0c74e0e34e3a0e0f23843bb3136dab5eec41444
amemeansrain <savrobartrobsav@gmail.com>
2025-06-29 01:25:37 +0300

    Fix path for JoinGroupMenu component in JoinGroup page

--- src/frontend/src/pages/JoinGroup.jsx
+++ src/frontend/src/pages/JoinGroup.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "/src/features/components/Header/Header.jsx";
-import JoinGroupMenu from "/src/features/components/JoinGroupMenu.jsx";
+import JoinGroupMenu from "/src/features/groups/components/JoinGroupMenu/JoinGroupMenu.jsx";
 
 export default function JoinGroup() {
   return <div>
