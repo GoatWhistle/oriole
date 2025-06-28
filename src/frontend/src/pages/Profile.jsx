daa50a71be30f4b3216d838e35f1e9568df9f55d
amemeansrain <savrobartrobsav@gmail.com>
2025-06-28 21:59:38 +0300

    Fix path for UserProfile component in Profile page

--- src/frontend/src/pages/Profile.jsx
+++ src/frontend/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "/src/features/components/Header/Header.jsx";
-import UserProfile from "/src/features/auth/components/UserProfile/UserProfile.jsx";
+import UserProfile from "/src/features/profile/components/UserProfile/UserProfile.jsx";
 
 export default function Profile() {
   return <div>
