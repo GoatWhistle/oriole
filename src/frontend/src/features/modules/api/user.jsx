a0b6f742444f888859f2691bf430e0ae53b9c9b5
amemeansrain <savrobartrobsav@gmail.com>
2025-06-30 10:30:41 +0300

    Change fetch requests to axios requests in API funcs for groups and modules

--- src/frontend/src/features/modules/api/user.jsx
+++ src/frontend/src/features/modules/api/user.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
+
 export const showModuleList = async () => {
-  const response = await fetch('/api/modules/');
-  return await response.json();
+  const response = await axios.get('/api/modules/');
+  return await response.data;
 };
