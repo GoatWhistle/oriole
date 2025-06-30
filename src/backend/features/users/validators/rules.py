902118c7933ab06cc370f10b6483d52e39f16bc8
artems <whard8y@gmail.com>
2025-06-30 11:29:41 +0300

    Delete an extra validator

--- src/backend/features/users/validators/rules.py
+++ src/backend/features/users/validators/rules.py
@@ -1,9 +1,7 @@
 from fastapi import HTTPException, status
 
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from features.users.schemas import UserAuthRead
-from features.groups.services.group import get_user_groups
 
 
 async def validate_activity_and_verification(
@@ -26,15 +24,3 @@ async def validate_activity_and_verification(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="User is unverified",
         )
-
-
-async def validate_user_has_no_groups(
-    session: AsyncSession, user_id: int, raise_exception: bool = True
-) -> bool:
-    groups = await get_user_groups(session=session, user_id=user_id)
-    if groups and raise_exception:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="User must leave all groups before deletion",
-        )
-    return not groups
