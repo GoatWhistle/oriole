f14f725525ff348d0da5a5641500b06b27b8455f
pupyk <pupykint@gmail.com>
2025-07-04 18:32:30 +0300

    Move out uvicorn.workers.UvicornWorker

--- src/backend/core/gunicorn/app.py
+++ src/backend/core/gunicorn/app.py
@@ -26,17 +26,3 @@ class Application(BaseApplication):
 
     def load(self):
         return self.app
-
-
-def get_options(
-    host: str,
-    port: int,
-    workers: int,
-    timeout: int,
-) -> dict:
-    return {
-        "bind": f"{host}:{port}",
-        "worker_class": "uvicorn.workers.UvicornWorker",
-        "workers": workers,
-        "timeout": timeout,
-    }
--- src/backend/core/gunicorn/options.py
+++ src/backend/core/gunicorn/options.py
@@ -2,12 +2,13 @@ def get_options(
     host: str,
     port: int,
     workers: int,
+    worker_class: str,
     timeout: int,
     reload: bool = False,
 ) -> dict:
     return {
         "bind": f"{host}:{port}",
-        "worker_class": "uvicorn.workers.UvicornWorker",
+        "worker_class": worker_class,
         "workers": workers,
         "timeout": timeout,
         "reload": reload,
