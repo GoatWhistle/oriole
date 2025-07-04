9b17b98f8cf9b7fa58122c7eb27de12dd8f7dc4a
Mikhail Khorokhorin <mikhailkhorokhorin@gmail.com>
2025-07-04 11:09:30 +0300

    Add migration

--- src/backend/alembic/env.py
+++ src/backend/alembic/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from core.config import settings
 from database import Base
+from features import *  # noqa
 
 from features import *  # noqa
 
