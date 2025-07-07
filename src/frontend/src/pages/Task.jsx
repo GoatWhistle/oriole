80b74afb7b8461b00849e67f157532bb08a46949
amemeansrain <savrobartrobsav@gmail.com>
2025-07-07 13:25:28 +0300

    Add test version for Task page, needs to be fixed

--- src/frontend/src/pages/Task.jsx
+++ src/frontend/src/pages/Task.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "/src/features/components/Header/Header.jsx";
-import TaskDetails from "/src/features/components/TaskDetails.jsx";
+import TaskDetails from "/src/features/tasks/components/TaskDetails/TaskDetails.jsx";
 
 export default function Task() {
   return <div>
