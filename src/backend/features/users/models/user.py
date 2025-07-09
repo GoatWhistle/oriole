23c26bceb55acc1b808b35166620b4433c9a2a9f
pupyk <pupykint@gmail.com>
2025-07-09 20:39:58 +0300

    Fix user's model

--- src/backend/features/users/models/user.py
+++ src/backend/features/users/models/user.py
@@ -29,7 +29,7 @@ class User(Base, IdIntPkMixin):
         default=0,
     )
 
-    messages: Mapped[list["Message"]] = relationship(back_populates="sender")
+    # messages: Mapped[list["Message"]] = relationship(back_populates="sender")
     profile: Mapped["UserProfile"] = relationship(
         back_populates="user", cascade="all, delete-orphan"
     )
